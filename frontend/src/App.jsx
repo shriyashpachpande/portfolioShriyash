8d4f026fc60b6cdd4ddfa5fdba0d7ba8c2cc81dd
shriyashpachpande <shriyashpachpande813@gmail.com>
2025-08-16 01:13:17 +0530

    Add files via upload

---
+++ frontend/src/App.jsx
@@ -0,0 +1,32 @@
+import { BrowserRouter } from "react-router-dom";
+
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
+import Footer from "./components/Footer";
+import CertificateGallery from "./components/CertificateGallery";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className='relative z-0 bg-primary'>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        {/* <Feedbacks /> */}
+        <CertificateGallery />
+        <div className='relative z-0'>
+          <Contact />
+
+          <StarsCanvas />
+          <Footer/>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
